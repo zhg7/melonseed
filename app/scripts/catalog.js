f47d60bf0b28a298679b5a0c3c564d446860f680
zhg7 <92540253+zhg7@users.noreply.github.com>
2023-01-31 13:37:48 +0100

    added full catalog, json file fixes

--- app/scripts/catalog.js
+++ app/scripts/catalog.js
@@ -1,4 +1,4 @@
-const JSON_FILE = "app/db/fruits.json";
+const JSON_FILE = "/app/db/fruits.json";
 
 let fruits;
 getFruits(JSON_FILE);
@@ -7,4 +7,56 @@ async function getFruits(path) {
     const response = await fetch(path);
     const result = await response.json();
     fruits = result;
+    showCatalog();
+}
+
+function showCatalog() {
+    const featuredSection = document.querySelector(".featured-fruits");
+    fruits.forEach(fruit => {
+        ({ fruit, price, image, origin } = fruit);
+        featuredSection.innerHTML += `
+    <div class="col">
+                        <div class="card">
+                            <div class="img-container">
+                                <img src="/app/assets/images/fruits/${image}" class="card-img-top img-fluid"
+                                alt="...">
+                            <div class="overlay text-light text-center">
+                                <h2 class="p-4">${fruit}</h2>
+                                Origen:
+                                <ul class="list-unstyled">
+                                    ${getCountryFlags(...origin)}
+                                </ul>
+                            </div>
+                            </div>     
+                            <div class="card-body">
+                                <h4 class="text-light text-center">${formatCurrency(price)}</h4>
+                                <div class="d-grid">
+                                    <button type="button" class="rounded-pill"><i class="bi bi-cart-plus-fill"></i>
+                                        Añadir</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`
+    })
+}
+
+function formatCurrency(price) {
+    const options = { style: 'currency', currency: 'EUR' };
+    const numberFormat = new Intl.NumberFormat('es-ES', options)
+    return numberFormat.format(price);
+}
+
+function getCountryName(iso){
+    const regionNames = new Intl.DisplayNames(['es'], { type: 'region' });
+    return regionNames.of(iso);
+}
+
+function getCountryFlags(...origin){
+    console.log(origin)
+    let countries = ""
+    origin.forEach(country => {
+        console.log(country)
+        countries += `<li><img src="https://flagsapi.com/${country.toUpperCase()}/flat/24.png"> ${getCountryName(country)}<li>`
+    })
+    return countries;
 }
